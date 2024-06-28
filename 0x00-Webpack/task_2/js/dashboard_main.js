a99046f0d609e4504efe3acb79348eb6681c5ea2
Taofeek Olusola <olusolataofeek@gmail.com>
2024-06-28 12:51:59 +0100

    task_2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,27 @@
+import $ from 'jquery';
+import debounce from 'lodash/debounce';
+import '../css/main.css';
+
+function addElements() {
+    const $logo = $('<div>').attr('id', 'logo');
+
+    const $dashboardTitle = $('<p>').text('Holberton Dashboard');
+    const $dashboardInfo = $('<p>').text('Dashboard data for the students');
+    const $startButton = $('<button>').text('Click here to get started');
+    const $countParagraph = $('<p>').attr('id', 'count');
+    const $copyright = $('<p>').text('Copyright - Holberton School');
+
+    $('body').append($dashboardTitle, $dashboardInfo, $startButton, $countParagraph, copyright);
+
+    $startButton.on('click', debounce(updateCounter, 300));
+}
+
+let clickCount = 0;
+function updateCounter() {
+    clickCount++;
+    $('#count').text(`${clickCount} clicks on the button`);
+}
+
+$(document).ready(function() {
+    addElements();
+});
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+module.exports = {
+    entry: './js/dashboard_main.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[hash].[ext]',
+                            outputPath: 'images'
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            optipng: {
+                                enabled: true,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    },
+                ],
+            },
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 9000,
+    },
+    mode: 'production'
+};
